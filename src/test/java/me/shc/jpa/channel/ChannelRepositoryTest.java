79b9be59339612b30bfb40b762a490da655b45e7
HyungCheolSim <sim34122@gmail.com>
2023-08-03 09:05:19 +0900

    day 4 적용 준비

--- src/test/java/me/shc/jpa/channel/ChannelRepositoryTest.java
+++ src/test/java/me/shc/jpa/channel/ChannelRepositoryTest.java
@@ -2,33 +2,36 @@ package me.shc.jpa.channel;
 
 import com.querydsl.core.types.Predicate;
 import java.util.Optional;
+import me.shc.jpa.common.RepositoryTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-@Transactional  // 실무에서 사용시 주의해야합니다. (테스트 대상 환경에 영향을 줌)
-@Rollback(value = false)
+@RepositoryTest
 class ChannelRepositoryTest {
 
   @Autowired
-  ChannelRepository channelRepository;
+  private ChannelRepository channelRepository;
 
+  @Test
+  void insertSelectChannelTest() {
+    // given
+    var newChannel = Channel.builder().name("new-channel").build();
+    // when
+    var savedChannel = channelRepository.save(newChannel);
+    // then
+    var foundChannel = channelRepository.findById(savedChannel.getId());
+    assert foundChannel.get().getId().equals(savedChannel.getId());
+  }
 
   @Test
   void queryDslTest() {
     // given
     var newChannel = Channel.builder().name("teasun").build();
     channelRepository.save(newChannel);
-
     Predicate predicate = QChannel.channel
         .name.equalsIgnoreCase("TEASUN");
-
     // when
     Optional<Channel> optional = channelRepository.findOne(predicate);
-
     // then
     assert optional.get().getName().equals(newChannel.getName());
   }
